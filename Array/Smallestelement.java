b0615ea0ef6d2c41ca24130f8fa2bed54be1f010
Krishna Singh <singhkrishna5099213@gmail.com>
2023-12-29 20:41:50 +0530

    4th commit

---
+++ Array/Smallestelement.java
@@ -0,0 +1,26 @@
+package Array;
+import java.util.*;
+public class Smallestelement {
+    public static int getLargest(int numbers[]){
+        int largest = Integer.MIN_VALUE;  // - infinity
+        int smallest = Integer.MAX_VALUE;
+
+        for(int i =0; i<numbers.length; i++ ){
+            if(largest <numbers[i]){
+                largest = numbers[i];
+
+            }
+            if(smallest > numbers[i]){
+                smallest = numbers[i];
+            
+            }
+        }
+        System.out.println("smallest numbers is : " + smallest);
+        return largest;
+        
+    }
+    public static void main(String args[]){
+        int numbers[] = {1 , 2 , 6 , 3 , 5};
+        System.out.println("Largest number is : " + getLargest(numbers));
+    }
+}
