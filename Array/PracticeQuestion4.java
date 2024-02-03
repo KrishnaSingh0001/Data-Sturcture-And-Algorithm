1cf5cd0775ffb213b5908f0f89e13eb359bd9025
Krishna Singh <singhkrishna5099213@gmail.com>
2024-02-03 22:05:38 +0530

    10th commit

---
+++ Array/PracticeQuestion4.java
@@ -0,0 +1,8 @@
+// Given n non - negative integer representing an elevation map where the width of each bar is 1 , 
+//compute how much water it can trap after raining 
+
+package Array;
+
+public class PracticeQuestion4 {
+    
+}
