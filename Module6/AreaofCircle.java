935c47e874ff1b5039fa1d8e92e3143fd3ccfaa5
Krishna Singh <singhkaran5099213.com>
2024-03-26 01:35:20 +0530

    6th

---
+++ Module6/AreaofCircle.java
@@ -0,0 +1,14 @@
+package Module6;
+import java.util.*;
+public class AreaofCircle {
+    public static void main(String args[]){
+     Scanner sc = new Scanner(System.in);
+     double pie = 3.14;
+     int  r = sc.nextInt();  
+
+     double a = 3.14 * r*r;
+     System.out.println(a);
+
+    }
+    
+}
---
+++ Module6/Var1Updt1.java
@@ -0,0 +1,19 @@
+package Module6;
+
+public class Var1Updt1 {
+    public static void main(String args[]){
+        int x = 5;
+        System.out.println(x);
+        
+        x = 7;
+        System.out.println(x);
+
+        x= x+ 6;
+        System.out.println(x);
+
+        x = x-20;
+        System.out.println(x);
+
+    }
+    
+}
