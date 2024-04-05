3ad41191c5677554cbba60ce048aebce3358351d
Krishna Singh <singhkaran5099213.com>
2024-04-05 17:37:35 +0530

    10th

---
+++ LoopsModule8/AirthmeticProgression.java
@@ -0,0 +1,15 @@
+package LoopsModule8;
+import java.util.*;
+public class AirthmeticProgression {
+    public static void main(String agrs[]){
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter n : ");
+        int n = sc.nextInt();
+        for (int i =1 ; i <= (2*n -1) ; i+=2){
+            System.out.println(i);
+
+        }
+
+    }
+    
+}
---
+++ LoopsModule8/table19.java
@@ -0,0 +1,15 @@
+package LoopsModule8;
+import java.util.*;
+public class table19 {
+    public static void main(String args []){
+        Scanner sc = new Scanner(System.in);
+        for (int i = 19 ; i<= 190 ; i++){
+            if(i %19==0 ){
+                System.out.println(i);
+            }
+        }
+        
+    }
+    
+    
+}
