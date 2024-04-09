11f3fd5bd66c3a14b9081768f8bc3613a231e8dd
Krishna Singh <singhkaran5099213.com>
2024-04-09 12:07:22 +0530

    12th commit

--- ArrayPW/CalculateSum.java
+++ ArrayPW/CalculateSum.java
@@ -5,6 +5,13 @@ public class CalculateSum {
     public static void main(String args[]) {
         int sum = 0;
         int[] num = {10, 20, 30, 40, 50, 60, 70};
+        for (int i = 0 ; i <6; i++){
+            sum += num[i];
+            // System.out.println("Sum of array element is : " + sum);
+        }
+
+        System.out.println("Sum of array element is : " + sum);
+        
 
     }
 }
